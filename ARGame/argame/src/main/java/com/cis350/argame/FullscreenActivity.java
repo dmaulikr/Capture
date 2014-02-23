f0ecb14b8389984674cc8d299bc9d0a674e11865
bastova <abastov@gmail.com>
2014-02-23 15:30:14 -0500

    leaflet shows map

--- ARGame/argame/src/main/java/com/cis350/argame/FullscreenActivity.java
+++ ARGame/argame/src/main/java/com/cis350/argame/FullscreenActivity.java
@@ -10,6 +10,38 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+//import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+//import org.osmdroid.views.MapController;
+//import org.osmdroid.views.MapView;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
@@ -17,6 +49,12 @@ import android.view.View;
  * @see SystemUiHider
  */
 public class FullscreenActivity extends Activity {
+
+    // global variables for osmdroid
+    //private MapView myOpenMapView;
+    //private MapController myMapController;
+
+
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -112,6 +150,92 @@ public class FullscreenActivity extends Activity {
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
+        // Showing the Map
+        // --------------- osmdroid --------------- //
+
+         /*  myOpenMapView = (MapView)findViewById(R.id.openmapview);
+        myOpenMapView.setBuiltInZoomControls(true);
+        myMapController = myOpenMapView.getController();
+        myMapController.setZoom(4);
+        myOpenMapView.setTileSource(TileSourceFactory.MAPQUESTOSM);
+        */
+
+        //--------- LEAFLET ----------------//
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+
+        // Permission to get user's geolocation
+        webSettings.setGeolocationEnabled(true);
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });
+
+        // Enable javascript in the html file of WebView
+        webSettings.setJavaScriptEnabled(true);
+
+        //----------File Location for loadUrl ------------//
+        //use "file:///android_asset/map.html" for device
+        //use "http://poroawards.net/Geolocation/map.html" for web or emulator, change hosting later
+
+        myWebView.loadUrl("http://poroawards.net/Geolocation/map.html");
+
+        // ---------------- OVERPASS API ------------------ //
+        // use XML queries and send them to interpreter as POST methods
+
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://overpass-api.de/api/interpreter");
+
+        try {
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            String data=
+                    "<?xml version=\"1.0\" encoding=\"UTF-8\"?><osm-script><!--" +
+                            "This is an example Overpass query."+
+                            "Try it out by pressing the Run button above!"+
+                            "You can find more examples with the Load tool."+
+                            "-->"+
+                            "<query type=\"node\">"+
+                            "<has-kv k=\"amenity\" v=\"drinking_water\"/>"+
+                            "<bbox-query s=\"41.88659196260802\" w=\"12.488558292388916\" n=\"41.89248629819397\" e=\"12.51119613647461\"/>" +
+                            "<!--this is auto-completed with the current map view coordinates.-->"+
+                            "</query>"+
+                            "<print/></osm-script>";
+            nameValuePairs.add(new BasicNameValuePair("form-data", data));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+
+            HttpEntity entity = response.getEntity();
+
+            // get the result from query in XML format
+            if (entity != null) {
+                InputStream instream = entity.getContent();
+
+                InputStreamReader is = new InputStreamReader(instream);
+                StringBuilder sb=new StringBuilder();
+                BufferedReader br = new BufferedReader(is);
+                String read = br.readLine();
+
+                while(read != null) {
+                    //System.out.println(read);
+                    sb.append(read);
+                    read =br.readLine();
+
+                }
+                String output = sb.toString(); // XML
+                System.out.println(output);
+            }
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
     }
 
     @Override

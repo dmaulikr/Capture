5986fd2acfa1e1f9c29168dc5c9337e71e900379
bastova <abastov@gmail.com>
2014-02-27 19:13:14 -0500

    shows polygons

--- ARGame/argame/src/main/java/com/cis350/argame/FullscreenActivity.java
+++ ARGame/argame/src/main/java/com/cis350/argame/FullscreenActivity.java
@@ -15,11 +15,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.http.HttpEntity;
@@ -31,6 +33,12 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 //import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 //import org.osmdroid.views.MapController;
 //import org.osmdroid.views.MapView;
@@ -45,6 +53,10 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Toast;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
@@ -53,6 +65,7 @@ import android.widget.Toast;
  */
 public class FullscreenActivity extends Activity {
 
+    WebView myWebView;
     // Web Interface to bind the javascript file
     public class WebAppInterface {
         Context mContext;
@@ -68,6 +81,7 @@ public class FullscreenActivity extends Activity {
             // Create a new HttpClient and Post Header
             httpclient = new DefaultHttpClient();
             httppost = new HttpPost("http://overpass-api.de/api/interpreter");
+
         }
 
         /** Show a toast from the web page */
@@ -77,29 +91,39 @@ public class FullscreenActivity extends Activity {
         }
 
         public void showBuildings(String bbox) {
-            Toast.makeText(mContext, bbox, Toast.LENGTH_SHORT).show();
+
+            // --------------- Data Structures ---------------- //
+            // key = way id, value = array of node ids
+            ArrayList<ArrayList<Integer>> polygons = new ArrayList<ArrayList<Integer>>();
+            // key = node id, value = array of latitude, longitude
+            HashMap<Integer, ArrayList<Float>> points = new HashMap<Integer, ArrayList<Float>>();
+            // ------------------------------------------------ //
+
+            //Toast.makeText(mContext, bbox, Toast.LENGTH_SHORT).show();
             String bounds[] = bbox.split(","); // w s e n
 
             // ---------------- OVERPASS API ------------------ //
             // use XML queries and send them to interpreter as POST methods
 
             try {
-                // Add your data
-              //  s          w
-              //  39.9531, -75.20255
-              //  n           e
-               // 39.9553128, -75.1969907
 
                 List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+
+                // Find all ways with key "building" and all their member nodes
                 String data=
-                "<?xml version=\"1.0\" encoding=\"UTF-8\"?><osm-script>"+
+
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?><osm-script timeout=\"900\" element-limit=\"1073741824\">"+
                 "<query type=\"way\">"+
                 "<has-kv k=\"building\" v=\"yes\"/>"+
                 "<bbox-query s=\""+bounds[1]+"\" w=\""+bounds[0]+"\" n=\""+bounds[3]+"\" e=\""+bounds[2]+"\"/>"+
-
-                "<!--this is auto-completed with the current map view coordinates.-->"+
                 "</query>"+
+                "<union>"+
+                "<item />"+
+                "<recurse type=\"way-node\"/>"+
+                "</union>"+
                 "<print/></osm-script>";
+
+
                 nameValuePairs.add(new BasicNameValuePair("form-data", data));
                 httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
@@ -107,8 +131,8 @@ public class FullscreenActivity extends Activity {
                 HttpResponse response = httpclient.execute(httppost);
 
                 HttpEntity entity = response.getEntity();
-
-                // get the result from query in XML format
+                String output = "";
+                // get the result from query in XML format and convert to string
                 if (entity != null) {
                     InputStream instream = entity.getContent();
 
@@ -123,14 +147,89 @@ public class FullscreenActivity extends Activity {
                         read =br.readLine();
 
                     }
-                    String output = sb.toString(); // XML
+                    output = sb.toString(); // XML
                     System.out.println(output);
                 }
+
+                // ------------ Parsing XML -------------- //
+                // Create a DOM element to parse XML
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                factory.setNamespaceAware(true); // allows access to localName
+                DocumentBuilder db = factory.newDocumentBuilder();
+                InputSource inStream = new InputSource();
+                inStream.setCharacterStream(new StringReader(output));
+                Document doc = db.parse(inStream);
+
+                // populate polygons
+                NodeList ways = doc.getElementsByTagName("way");
+                for (int i = 0; i < ways.getLength(); i++) {
+                    Node w_item = ways.item(i);
+                    int w_id = Integer.parseInt(w_item.getAttributes().item(0).getNodeValue()); // way id
+                    NodeList w_nodes = w_item.getChildNodes();
+
+                    ArrayList<Integer> node_ids = new ArrayList<Integer>();
+
+                    for(int j = 0; j < w_nodes.getLength(); j++) {
+                        Node w_child = w_nodes.item(j);
+                        if( w_child.hasAttributes()) {
+                            String local_name = w_child.getLocalName();
+                            // get id of the node that belongs to this way
+                            if( local_name.compareTo("nd") == 0) {
+                                 int n_id = Integer.parseInt(w_child.getAttributes().item(0).getNodeValue()); // node id
+                                 node_ids.add(n_id);
+                            }
+                        }
+                    }
+
+                    polygons.add(node_ids);
+                }
+
+                // populate points
+                NodeList nodes = doc.getElementsByTagName("node");
+                for (int i = 0; i < nodes.getLength(); i++) {
+                    Node n_item = nodes.item(i);
+                    int n_id = Integer.parseInt(n_item.getAttributes().item(0).getNodeValue()); // node id
+                    float latitude = Float.parseFloat(n_item.getAttributes().item(1).getNodeValue()); // lat
+                    float longitude = Float.parseFloat(n_item.getAttributes().item(2).getNodeValue()); // lon
+
+                    ArrayList<Float> node_coords = new ArrayList<Float>();
+                    node_coords.add(latitude);
+                    node_coords.add(longitude);
+
+                    points.put(n_id, node_coords);
+                }
+
             } catch (ClientProtocolException e) {
                 // TODO Auto-generated catch block
             } catch (IOException e) {
                 // TODO Auto-generated catch block
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            } catch (SAXException e) {
+                e.printStackTrace();
             }
+
+            // --------------- to Leaflet --------------- //
+            // send all polygon data to Leaflet to draw on the map
+
+            String point_data = "";
+            for( int i = 0; i < polygons.size(); i ++) {
+                ArrayList<Integer> polygon = polygons.get(i);
+                for( int j = 0; j < polygon.size(); j ++ ) {
+                    ArrayList<Float> lat_lon = points.get(polygon.get(j));
+                    if( lat_lon != null ) {
+                        point_data += lat_lon.get(0) + "," + lat_lon.get(1);
+                    }
+                    if(j < polygon.size() - 1) {
+                        point_data += ";";
+                    }
+                }
+                myWebView.loadUrl("javascript:drawPolygonFromPoints(\""+point_data+"\")");
+                point_data = "";
+            }
+
+            //String msg = "Hello from Android";
+            //myWebView.loadUrl("javascript:wave()");
         }
     }
     // end Web Interface
@@ -248,8 +347,9 @@ public class FullscreenActivity extends Activity {
 
         //--------- LEAFLET ----------------//
 
-        WebView myWebView = (WebView) findViewById(R.id.webview);
-
+        myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.clearCache(true); // clear the cached javascript
+        myWebView.loadUrl("http://poroawards.net/Geolocation/map.html");
         // add web interface
         myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
 
@@ -262,6 +362,7 @@ public class FullscreenActivity extends Activity {
                 callback.invoke(origin, true, false);
             }
         });
+        webSettings.setDomStorageEnabled(true);
 
         // Enable javascript in the html file of WebView
         webSettings.setJavaScriptEnabled(true);
@@ -269,8 +370,6 @@ public class FullscreenActivity extends Activity {
         //----------File Location for loadUrl ------------//
         //use "file:///android_asset/map.html" for device
         //use "http://poroawards.net/Geolocation/map.html" for web or emulator, change hosting later
-
-        myWebView.loadUrl("http://poroawards.net/Geolocation/map.html");
 
     }
 

99f570fef05bcf24b380243f9cf9f6771bcefe9d
Alan A <aaquino@seas.upenn.edu>
2014-04-16 14:29:51 -0400

    Forgot assets from last commit.

--- ARGame/argame/src/main/java/com/cis350/argame/SettingsActivity.java
+++ ARGame/argame/src/main/java/com/cis350/argame/SettingsActivity.java
@@ -1,7 +1,15 @@
 package com.cis350.argame;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Alan on 4/16/2014.
  */
-public class SettingsActivity {
+public class SettingsActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_layout);
+    }
 }

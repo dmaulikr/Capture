a6e2e46330f05461f8fe365f4b48681d06a86c15
Alan A <aaquino@seas.upenn.edu>
2014-04-21 22:17:11 -0400

    Created methods and files for uploading profile picture. Need to test.

--- ARGame/argame/src/main/java/com/cis350/argame/GameActivity.java
+++ ARGame/argame/src/main/java/com/cis350/argame/GameActivity.java
@@ -4,44 +4,34 @@ import com.cis350.argame.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Context;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.view.MotionEvent;
 import android.view.View;
 
-import java.io.IOException;
-import java.io.StringReader;
+import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.List;
 
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-//import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-//import org.osmdroid.views.MapController;
-//import org.osmdroid.views.MapView;
-
-
-import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.GeolocationPermissions;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -54,6 +44,14 @@ public class GameActivity extends Activity {
     private TextView coinsText;
     private TextView armiesText;
     private TextView nameText;
+    private ImageView profilePic;
+    private int PIC_SQUARE_WIDTH;
+
+    private Uri mImageCaptureUri;
+
+    private static final int PICK_FROM_CAMERA = 1;
+    private static final int CROP_FROM_CAMERA = 2;
+    private static final int PICK_FROM_FILE = 3;
 
     WebView myWebView;
 
@@ -139,22 +137,22 @@ public class GameActivity extends Activity {
         TextView coinsText = (TextView)findViewById(R.id.coinstext);
         TextView armiesText = (TextView)findViewById(R.id.armiestext);
         TextView nameText = (TextView)findViewById(R.id.playerName);
+        ImageView profilePic = (ImageView)findViewById(R.id.profilePicture);
 
         this.coinsText = coinsText;
         this.armiesText = armiesText;
         this.nameText = nameText;
+        this.profilePic = profilePic;
+        this.PIC_SQUARE_WIDTH = profilePic.getWidth();
 
         Integer currentCoins = PlayerProfile.getGold();
         Integer currentArmies = PlayerProfile.getArmy();
 
-        coinsText.setText(currentCoins.toString() + "\nCoins");
-        armiesText.setText(currentArmies.toString() + "\nArmies");
-        nameText.setText(PlayerProfile.getName());
+        coinsText.setText(currentCoins.toString() + "\nCoins"); // Set coins to player amt.
+        armiesText.setText(currentArmies.toString() + "\nArmies"); // Same with armies.
+        nameText.setText(PlayerProfile.getName()); // Set player name.
+        // TODO: Get the player picture from profile if applicable, else prompt the player for a pic
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
         showMap();
     }
 
@@ -237,7 +235,6 @@ public class GameActivity extends Activity {
     // Player settings methods
 
     public void onBuyArmiesClick(View v) {
-        //Toast.makeText(this, "Buy armies unimplemented!", Toast.LENGTH_LONG).show();
         if(PlayerProfile.getGold() >= 10) {
             PlayerProfile.ARMY += 1;
             PlayerProfile.GOLD -= 10;
@@ -254,11 +251,146 @@ public class GameActivity extends Activity {
     }
 
     public void onProfilePictureClick(View v) {
-        Toast.makeText(this, "Picture changing unimplemented!", Toast.LENGTH_LONG).show();
+        final String[] items = new String[] {"Take from camera", "Select from gallery"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.select_dialog_item, items);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Select Image");
+        builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) { // Pick from camera
+                if (item == 0) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),
+                            "tempAvatar" + String.valueOf(System.currentTimeMillis()) + ".jpg"));
+
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+
+                    try {
+                        intent.putExtra("return-data", true);
+                        startActivityForResult(intent, PICK_FROM_CAMERA);
+                    } catch (ActivityNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                } else { // Pick from a file
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(
+                            intent, "Complete action using"), PICK_FROM_FILE);
+                }
+            }
+        });
     }
 
     @Override
-    protected  void onStop() {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) return;
+
+        switch (requestCode) {
+            case PICK_FROM_CAMERA:
+                doCrop();
+                break;
+            case PICK_FROM_FILE:
+                mImageCaptureUri = data.getData();
+                doCrop();
+                break;
+            case CROP_FROM_CAMERA:
+                Bundle extras = data.getExtras();
+                if (extras != null) {
+                    Bitmap photo = extras.getParcelable("data");
+                    profilePic.setImageBitmap(photo);
+                }
+                File f = new File(mImageCaptureUri.getPath());
+                if (f.exists()) {
+                    f.delete(); // Delete the temporary file.
+                }
+                break;
+        }
+    }
+
+    /**
+     * Crop the picture passed in by the user.
+     */
+    private void doCrop() {
+        final ArrayList<CropOption> cropOptions = new ArrayList<CropOption>();
+
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setType("image/*");
+
+        List<ResolveInfo> list = getPackageManager().queryIntentActivities(intent, 0);
+
+        int size = list.size();
+
+        if (size == 0) {
+            Toast.makeText(this, "Cannot find image crop app", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            intent.setData(mImageCaptureUri);
+
+            intent.putExtra("outputX", PIC_SQUARE_WIDTH);
+            intent.putExtra("outputY", PIC_SQUARE_WIDTH);
+            intent.putExtra("aspectX", 1);
+            intent.putExtra("aspectY", 1);
+            intent.putExtra("scale", true);
+            intent.putExtra("return-data", true);
+
+            if (size == 1) {
+                Intent i = new Intent(intent);
+                ResolveInfo res = list.get(0);
+
+                i.setComponent(new ComponentName(res.activityInfo.packageName,
+                        res.activityInfo.name));
+
+                startActivityForResult(i, CROP_FROM_CAMERA);
+            } else {
+                for (ResolveInfo res : list) {
+                    final CropOption co = new CropOption();
+                    co.title = getPackageManager().getApplicationLabel(
+                            res.activityInfo.applicationInfo);
+                    co.icon = getPackageManager().getApplicationIcon(
+                            res.activityInfo.applicationInfo);
+                    co.appIntent = new Intent(intent);
+
+                    co.appIntent.setComponent(new ComponentName(
+                            res.activityInfo.packageName, res.activityInfo.name));
+
+                    cropOptions.add(co);
+                }
+
+                CropOptionAdapter adapter = new CropOptionAdapter(
+                        getApplicationContext(), cropOptions);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Choose Crop App");
+                builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int item) {
+                        startActivityForResult(cropOptions.get(item).appIntent, CROP_FROM_CAMERA);
+                    }
+                });
+
+                builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        if (mImageCaptureUri != null) {
+                            getContentResolver().delete(mImageCaptureUri, null, null);
+                            mImageCaptureUri = null;
+                        }
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+
+                alert.show();
+            }
+        }
+    }
+
+    @Override
+    protected void onStop() {
         super.onStop();
 
         ParseManager.updateCurrentUserArmy(PlayerProfile.ARMY, PlayerProfile.GOLD);
